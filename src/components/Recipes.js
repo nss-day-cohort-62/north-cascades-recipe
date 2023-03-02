473788e49f88032d7556d93a03a41971cb474f1c
hannahhall <hannahkhall13@gmail.com>
2023-03-02 11:44:26 -0600

    list of recipes

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,13 +1,33 @@
 // TODO: This component should show a list of all recipes and
 // include a select element to filter recipes by category
 
+import { useState, useEffect } from 'react'
 import './Recipes.css'
 
 export const Recipes = () => {
   // TODO: Create State for recipes, categories, filteredRecipes, categoryChoice
-
+  const [recipes, setRecipes] = useState([])
+  const [categories, setCategories] = useState([])
+  const [filteredRecipes, setFilteredRecipes] = useState([])
+  const [categoryChoice, setCategoryChoice] = useState(0)
   // TODO: Create a useEffect that fetches recipes and categories
+  useEffect(
+    () => {
+      console.log('Page Loaded')
+      fetch('http://localhost:8088/recipes')
+        .then(response => response.json())
+        .then((data) => {
+          setRecipes(data)
+          setFilteredRecipes(data)
+        })
 
+      fetch('http://localhost:8088/categories')
+        .then(response => response.json())
+        .then(data => {
+          setCategories(data)
+        })
+    }, []
+  )
 
   // TODO: Create a useEffect that updates the filteredRecipes state if categoryChoice changes
 
@@ -22,7 +42,16 @@ export const Recipes = () => {
 
       <div className="recipe-container">
         {/* TODO: Display the filteredRecipes */}
-
+        <article class="filteredrecipes">
+          {
+            filteredRecipes.map((recipe) => {
+              return <section className="recipe">
+                <header>{recipe.name}</header>
+                <p>{recipe.recipeText}</p>
+              </section>
+            })
+          }
+          </article>
       </div>
     </>
   )
